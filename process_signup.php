81839a221b70f1f30970bca1a65cf184bc87ef79
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-05-24 15:19:38 +0200

    better validations for signup

--- process_signup.php
+++ process_signup.php
@@ -1,6 +1,5 @@
 <?php
 header('Content-Type: application/json');
-
 require_once 'db.php';
 
 $response = [
@@ -17,20 +16,50 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $confirmPassword = $_POST['confirmPassword'];
     $phone = trim($_POST['phone']);
 
-    if (strlen($name) < 2 || strlen($surname) < 2) {
-        $response['errors'][] = "Emri dhe mbiemri duhet të kenë të paktën 2 shkronja.";
+    if (empty($name)) {
+        $response['errors'][] = "Emri është i detyrueshëm.";
+    } elseif (strlen($name) < 2 || strlen($name) > 50) {
+        $response['errors'][] = "Emri duhet të ketë midis 2 dhe 50 shkronjave.";
+    } elseif (!preg_match('/^[\p{L}\s\'-]+$/u', $name)) {
+        $response['errors'][] = "Emri përmban karaktere të palejuara (vetëm shkronja, hapësira, apostrofë ose vizë).";
     }
 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (empty($surname)) {
+        $response['errors'][] = "Mbiemri është i detyrueshëm.";
+    } elseif (strlen($surname) < 2 || strlen($surname) > 50) {
+        $response['errors'][] = "Mbiemri duhet të ketë midis 2 dhe 50 shkronjave.";
+    } elseif (!preg_match('/^[\p{L}\s\'-]+$/u', $surname)) {
+        $response['errors'][] = "Mbiemri përmban karaktere të palejuara (vetëm shkronja, hapësira, apostrofë ose vizë).";
+    }
+
+    if (empty($email)) {
+        $response['errors'][] = "Email-i është i detyrueshëm.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $response['errors'][] = "Email-i nuk është i vlefshëm.";
+    } elseif (strlen($email) > 100) {
+        $response['errors'][] = "Email-i nuk mund të jetë më i gjatë se 100 karaktere.";
     }
 
-    if (strlen($password) < 6) {
-        $response['errors'][] = "Fjalëkalimi duhet të ketë të paktën 6 karaktere.";
+    if (empty($password)) {
+        $response['errors'][] = "Fjalëkalimi është i detyrueshëm.";
+    } elseif (strlen($password) < 8) {
+        $response['errors'][] = "Fjalëkalimi duhet të ketë të paktën 8 karaktere.";
+    } elseif (!preg_match('/[A-Z]/', $password)) {
+        $response['errors'][] = "Fjalëkalimi duhet të përmbajë të paktën një shkronjë të madhe.";
+    } elseif (!preg_match('/[0-9]/', $password)) {
+        $response['errors'][] = "Fjalëkalimi duhet të përmbajë të paktën një numër.";
+    } elseif (!preg_match('/[^A-Za-z0-9]/', $password)) {
+        $response['errors'][] = "Fjalëkalimi duhet të përmbajë të paktën një karakter special (e.g., !@#$%^&*).";
     }
 
     if ($password !== $confirmPassword) {
         $response['errors'][] = "Fjalëkalimet nuk përputhen.";
+    }
+
+    if (empty($phone)) {
+        $response['errors'][] = "Numri i telefonit është i detyrueshëm.";
+    } elseif (!preg_match('/^\+?[0-9]{8,15}$/', $phone)) {
+        $response['errors'][] = "Numri i telefonit duhet të jetë midis 8 dhe 15 shifrave (mund të fillojë me +).";
     }
 
     if (empty($response['errors'])) {
@@ -50,14 +79,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if ($stmt->execute()) {
                 $response['success'] = true;
                 $response['redirect'] = 'login.php';
-                $response['message'] = "Regjistrimi u krye me sukses!";
             } else {
                 $response['errors'][] = "Dështoi regjistrimi. Ju lutemi provoni përsëri.";
             }
-
             $stmt->close();
         }
-
         $checkStmt->close();
     }
 

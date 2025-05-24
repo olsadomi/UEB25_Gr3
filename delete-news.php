5130e8c3678597e19a2d8ef9dc698035e64f97bb
bleronbaftiu21 <bleronbaftiu2@gmail.com>
2025-05-25 01:52:38 +0200

    shtimi i edit dhe delete butonit

---
+++ delete-news.php
@@ -0,0 +1,22 @@
+<?php
+require 'db.php';
+
+if(isset($_GET['id'])){
+    $id = intval($_GET['id']);
+
+    $stmt = $conn->prepare("DELETE FROM news WHERE id = ?");
+    $stmt->bind_param("i" , $id);
+
+    if($stmt->execute()){
+        header("Location: news-admin.php?message=deleted");
+        exit();
+    } else {
+        echo "Gabim gjate fshirjes se lajmit.";
+    }
+
+    $stmt->close();
+} else {
+    echo "ID e lajmit nuk eshte dhene";
+}
+
+?>

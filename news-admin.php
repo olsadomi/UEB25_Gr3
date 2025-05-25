1e9f87263d9ea92d1b77a3262e58ae105fa201f5
bleronbaftiu21 <bleronbaftiu2@gmail.com>
2025-05-25 20:58:55 +0200

    fixed

--- news-admin.php
+++ news-admin.php
@@ -1,6 +1,12 @@
 <?php session_start(); ?>
 <?php
 require 'db.php';
+
+if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'admin') {
+    header('Location: login.php');
+    exit;
+}
+
 $result = $conn->query("SELECT id, title, created_at, category FROM news ORDER BY created_at DESC");
 ?>
 
